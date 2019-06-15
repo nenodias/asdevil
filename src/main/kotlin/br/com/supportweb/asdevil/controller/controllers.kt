320b9fd73ec9daf42575d04414362e2f944f7352
Horácio Dias Baptista Neto <horacio.dias92@gmail.com>
2019-06-14 21:03:57 -0300

    Adicionando exemplo com eventos e streams

--- src/main/kotlin/br/com/supportweb/asdevil/controller/controllers.kt
+++ src/main/kotlin/br/com/supportweb/asdevil/controller/controllers.kt
@@ -3,9 +3,13 @@ package br.com.supportweb.asdevil.controller
 import br.com.supportweb.asdevil.document.Post
 import br.com.supportweb.asdevil.service.PostService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.util.function.Tuple2
+import java.awt.PageAttributes
+import java.time.Duration
 
 @RestController
 class PostController {
@@ -36,6 +40,14 @@ class PostController {
     @DeleteMapping("/post/{id}")
     fun delete(@PathVariable id:String): Mono<Post> {
         return postService.delete(id)
+    }
+
+    @GetMapping("/events", produces = arrayOf(MediaType.TEXT_EVENT_STREAM_VALUE))
+    fun events(): Flux<Tuple2<Long, Post>> {
+        val interval:Flux<Long> = Flux.interval(Duration.ofSeconds(5))
+        val events = postService.findAll()
+        print("Passou por aqui")
+        return Flux.zip(interval, events)
     }
 
 }
